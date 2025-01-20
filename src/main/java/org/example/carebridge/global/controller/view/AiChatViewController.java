b57dd483eaaecc0abee81c78795505fa5b9ebbca
jgw1202 <jgw1202@naver.com>
2025-01-20 20:13:40 +0900

    refac : viewController 디렉토리 리팩토링

--- src/main/java/org/example/carebridge/global/controller/view/AiChatViewController.java
+++ src/main/java/org/example/carebridge/global/controller/view/AiChatViewController.java
@@ -1,10 +1,10 @@
-package org.example.carebridge.domain.aichat.controller;
+package org.example.carebridge.global.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class AiChatViewController {
 
     @GetMapping("/ai")
     public String aichat() {
