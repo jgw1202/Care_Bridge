037b834d7a22ff3f42ce208eaa5a4d3c4a60d307
jgw1202 <jgw1202@naver.com>
2025-01-22 16:42:30 +0900

    feat/test : redisson 활용하여 좋아요 동시성 제어 기능 및 테스트 구현

--- src/main/java/org/example/carebridge/domain/like/repository/LikeRepository.java
+++ src/main/java/org/example/carebridge/domain/like/repository/LikeRepository.java
@@ -1,6 +1,8 @@
 package org.example.carebridge.domain.like.repository;
 
+import org.example.carebridge.domain.board.entity.Board;
 import org.example.carebridge.domain.like.entity.Like;
+import org.example.carebridge.domain.user.entity.User;
 import org.example.carebridge.global.exception.ExceptionType;
 import org.example.carebridge.global.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,4 +27,23 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
         // ID를 사용하여 Like 엔티티를 조회하고, Optional이 비어있으면 예외를 던짐
         return findById(id).orElseThrow(() -> new NotFoundException(ExceptionType.LIKE_NOT_FOUND));
     }
+
+    /**
+     * 특정 보드와 사용자에 대해 좋아요가 존재하는지 확인하는 메서드.
+     *
+     * @param board 좋아요가 속한 보드
+     * @param user 좋아요를 누른 사용자
+     * @return 좋아요가 존재하면 true, 그렇지 않으면 false
+     */
+    default boolean existsByBoardAndUser(Board board, User user) {
+        return findByBoardAndUser(board, user) != null;
+    }
+
+    /**
+     * 특정 보드와 사용자로 좋아요 엔티티를 조회하는 메서드.
+     * @param board 좋아요가 속한 보드
+     * @param user 좋아요를 누른 사용자
+     * @return Like 엔티티 또는 null
+     */
+    Like findByBoardAndUser(Board board, User user);
 }
