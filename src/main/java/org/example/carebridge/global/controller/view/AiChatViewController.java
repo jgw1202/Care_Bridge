36cd4f3ef217eff4e6d0ffb2c63013264ac146fb
jgw1202 <jgw1202@naver.com>
2025-01-28 17:17:36 +0900

    fix : AI 채팅 UI 수정 및 view 구현

--- src/main/java/org/example/carebridge/global/controller/view/AiChatViewController.java
+++ src/main/java/org/example/carebridge/global/controller/view/AiChatViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AiChatViewController {
 
-    @GetMapping("/ai")
+    @GetMapping("/aichat")
     public String aichat() {
         return "aichat";
     }
