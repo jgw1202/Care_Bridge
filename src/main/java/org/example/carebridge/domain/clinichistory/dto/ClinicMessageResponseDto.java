0de00d7417d52f08539ccebdda41d144b970ca74
Mini9709 <tjdals9709@naver.com>
2025-02-04 19:41:52 +0900

    feat : 진료내역 메세지 조회 기능 추가

---
+++ src/main/java/org/example/carebridge/domain/clinichistory/dto/ClinicMessageResponseDto.java
@@ -0,0 +1,12 @@
+package org.example.carebridge.domain.clinichistory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ClinicMessageResponseDto {
+
+    private String name;
+    private String message;
+}
